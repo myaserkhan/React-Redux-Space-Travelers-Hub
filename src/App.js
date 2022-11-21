f774c1711d841bac984e39ff3c7535b7d930254d
john <kiokoj81@gmail.com>
2022-11-21 17:16:49 +0300

    create nav in the header component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
+import Header from "./components/Header";
 function App() {
   return (
     <div className="App">
+      <header />
       <h1>Space Taverlers</h1>
     </div>
   );
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <nav>
+      <h1>Header</h1>
+    </nav>
+  )
+}
