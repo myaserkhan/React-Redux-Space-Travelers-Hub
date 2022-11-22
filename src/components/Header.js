379a18d6e3f0ff614fbc8c6386443d3d842d98fc
Yasir Khan <khanyaser007@gmail.com>
2022-11-22 14:02:10 +0500

    Create a Header component with navigation with styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import logo from '../images/planet.png';
+// import './stylesheets/Header.css';
 
-export default function Header() {
-  return (
+const navLinkStyles = ({ isActive }) => ({
+  fontWeight: isActive ? 'bold' : 'normal',
+  textDecoration: isActive ? 'underline' : 'none',
+});
+
+const Header = () => (
+  <header>
+    <div className="logo">
+      <img className="space-logo" src={logo} alt="logo" />
+      <span className="title-logo">Space Travelers&apos;  Hub</span>
+    </div>
     <nav>
-      <h1>Header</h1>
+      <ul>
+        <NavLink to="/" style={navLinkStyles}><span className="rockets">Rockets</span></NavLink>
+        <NavLink to="missions" style={navLinkStyles}><span className="missions">Missions</span></NavLink>
+        <NavLink to="myprofile" style={navLinkStyles}><span className="myprofile">My Profile</span></NavLink>
+      </ul>
     </nav>
-  );
-}
+  </header>
+);
+
+export default Header;
