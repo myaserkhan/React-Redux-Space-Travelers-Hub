5163bc01eb01562d94c315163d8d47d2475a654c
Yasir Khan <khanyaser007@gmail.com>
2022-11-22 13:51:50 +0500

    Create a Header component with navigation with styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import { Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
+
+import Rockets from './components/Rockets';
+import Missions from './components/Missions';
+import Myprofile from './components/Myprofile';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <h1>Space Taverlers</h1>
+      <Routes>
+        <Route path="/" element={<Rockets />} />
+        <Route path="missions" element={<Missions />} />
+        <Route path="myprofile" element={<Myprofile />} />
+      </Routes>
     </div>
   );
 }
