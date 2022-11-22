ecaba260de7fd7a2e5e05940dfe011de9e4ba447
Yasir Khan <khanyaser007@gmail.com>
2022-11-22 19:11:01 +0500

    Config store for rockets state slice

---
+++ src/redux/configStore.js
@@ -0,0 +1,12 @@
+import { configureStore } from '@reduxjs/toolkit';
+import logger from 'redux-logger';
+import rocketReducer from './rockets/RocketsSlice';
+
+const store = configureStore({
+  reducer: {
+    rockets: rocketReducer,
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+});
+
+export default store;
