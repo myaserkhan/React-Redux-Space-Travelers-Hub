52f119114305400c0539f14a8315ab44ac0ddab1
Yasir Khan <khanyaser007@gmail.com>
2022-11-24 18:08:39 +0500

    Reserve rockets in the profile

--- src/components/Myprofile/Myprofile.js
+++ src/components/Myprofile/Myprofile.js
@@ -4,12 +4,18 @@ import {
   unjoinMission,
   selectAllmissions,
 } from '../../redux/missions/MissionsSlice';
+import { cancelRocket } from '../../redux/rockets/RocketsSlice';
 
 function Myprofile() {
   const dispatch = useDispatch();
-
+  const rockets = useSelector((state) => state.rockets.filter((el) => el.rocketReserved === true));
   const reservation = useSelector(selectAllmissions);
   const missiondat = reservation.filter((item) => item.missionjoin === true);
+
+  const cancelHandler = (e) => {
+    const data = e.target.id;
+    dispatch(cancelRocket(data));
+  };
 
   const cancelmissionHandler = (e) => {
     const misiondata = e.target.id;
@@ -43,6 +49,24 @@ function Myprofile() {
       </div>
       <div className="rockets">
         <h1>My Rockets</h1>
+        <div className="itemsList">
+          {!rockets.length ? (<li>There are no rockets reserved.</li>) : null}
+          {rockets && rockets.map((el) => (
+            <div className="listBox" key={el.rocketId}>
+              <h2 className="listName">
+                {el.rocketName}
+              </h2>
+              <button className="readMore" type="button">
+                <a href={el.rocketWiki}>
+                  Read More
+                </a>
+              </button>
+              <button className="remBtn" type="button" id={el.rocketId} onClick={cancelHandler}>
+                Cancel
+              </button>
+            </div>
+          ))}
+        </div>
       </div>
     </section>
   );
